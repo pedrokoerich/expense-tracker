73410ad73debb95454dcb6109c46933164682a07
Pedro H. Koerich <pedro.koerich@totvs.com.br>
2025-05-14 11:44:09 -0300

    feat: dados armazenados

---
+++ src/data/items.ts
@@ -0,0 +1,34 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+    {
+        date: new Date(2025, 4, 1),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12,
+    },
+    {
+        date: new Date(2025, 4, 2),
+        category: 'food',
+        title: 'Burger King',
+        value: 28.99,
+    },
+    {
+        date: new Date(2025, 4, 3),
+        category: 'rent',
+        title: 'Aluguel',
+        value: 2300,
+    },
+    {
+        date: new Date(2025, 4, 4),
+        category: 'Salary',
+        title: 'Salário',
+        value: 3500,
+    },
+    {
+        date: new Date(2025, 4, 5),
+        category: 'food',
+        title: 'Dominos',
+        value: 30.00,
+    },
+]
